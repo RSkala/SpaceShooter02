5a245e73917c00ce7efbdf331180f39e5e52e04d
RSkala <RichardSkala@gmail.com>
2024-07-09 21:27:28 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SpaceShooter02
+ 2D Twin Stick Shooter built in Unreal Engine 5
---
+++ Source/SpaceShooter02/SpaceShooter02.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SpaceShooter02.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpaceShooter02, "SpaceShooter02" );
