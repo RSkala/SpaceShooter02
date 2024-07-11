f77fc492d473291aaf26561427c0c6e56febd79d
RSkala <RichardSkala@gmail.com>
2024-07-11 14:50:10 -0700

    Change log category verbosity for player ship pawn. added to defaultengine.ini

--- Source/SpaceShooter02/Private/PlayerShipPawn.cpp
+++ Source/SpaceShooter02/Private/PlayerShipPawn.cpp
@@ -12,12 +12,13 @@
 #include "InputActionValue.h"
 #include "PaperSpriteComponent.h"
 
-DEFINE_LOG_CATEGORY_STATIC(LogPlayerShipPawn, Log, All)
+DEFINE_LOG_CATEGORY_STATIC(LogPlayerShipPawn, Warning, All)
+DEFINE_LOG_CATEGORY_STATIC(LogPlayerShipPawnInput, Warning, All)
 
 // Sets default values
 APlayerShipPawn::APlayerShipPawn()
 {
-	UE_LOG(LogPlayerShipPawn, Log, TEXT("APlayerShipPawn::APlayerShipPawn - %s"), *GetName());
+	UE_LOG(LogPlayerShipPawn, Verbose, TEXT("APlayerShipPawn::APlayerShipPawn - %s"), *GetName());
 
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -49,7 +50,7 @@ void APlayerShipPawn::Tick(float DeltaTime)
 // Called to bind functionality to input
 void APlayerShipPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	UE_LOG(LogPlayerShipPawn, Log, TEXT("APlayerShipPawn::SetupPlayerInputComponent - %s"), *GetName());
+	UE_LOG(LogPlayerShipPawnInput, Verbose, TEXT("APlayerShipPawn::SetupPlayerInputComponent - %s"), *GetName());
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
@@ -79,7 +80,7 @@ void APlayerShipPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 // Called when the game starts or when spawned
 void APlayerShipPawn::BeginPlay()
 {
-	UE_LOG(LogPlayerShipPawn, Log, TEXT("APlayerShipPawn::BeginPlay - %s"), *GetName());
+	UE_LOG(LogPlayerShipPawn, Verbose, TEXT("APlayerShipPawn::BeginPlay - %s"), *GetName());
 	Super::BeginPlay();
 
 	// Setup Input Mapping Context
@@ -102,47 +103,46 @@ void APlayerShipPawn::BeginPlay()
 
 void APlayerShipPawn::KeyboardMoveTriggered(const FInputActionValue& InputActionValue)
 {
-	UE_LOG(LogPlayerShipPawn, Log, TEXT("APlayerShipPawn::KeyboardMoveTriggered - %s"), *GetName());
 	FVector2D MoveTriggeredInput = InputActionValue.Get<FVector2D>();
 	MovementDirection = MoveTriggeredInput.GetSafeNormal();
+	UE_LOG(LogPlayerShipPawnInput, Verbose, TEXT("APlayerShipPawn::KeyboardMoveTriggered - MovementDirection: %s"), *MovementDirection.ToString());
 }
 
 void APlayerShipPawn::KeyboardMoveCompleted(const FInputActionValue& InputActionValue)
 {
-	UE_LOG(LogPlayerShipPawn, Log, TEXT("APlayerShipPawn::KeyboardMoveCompleted - %s"), *GetName());
 	MovementDirection = FVector2D::ZeroVector;
+	UE_LOG(LogPlayerShipPawnInput, Verbose, TEXT("APlayerShipPawn::KeyboardMoveCompleted - MovementDirection: %s"), *MovementDirection.ToString());
 }
 
 void APlayerShipPawn::GamepadMoveTriggered(const FInputActionValue& InputActionValue)
 {
 	FVector2D MoveTriggeredInput = InputActionValue.Get<FVector2D>();
 	MovementDirection = MoveTriggeredInput.GetSafeNormal();
-
-	UE_LOG(LogPlayerShipPawn, Log, TEXT("APlayerShipPawn::GamepadMoveTriggered - MovementDirection: %s"), *MovementDirection.ToString());
+	UE_LOG(LogPlayerShipPawnInput, Verbose, TEXT("APlayerShipPawn::GamepadMoveTriggered - MovementDirection: %s"), *MovementDirection.ToString());
 }
 
 void APlayerShipPawn::GamepadMoveCompleted(const FInputActionValue& InputActionValue)
 {
 	MovementDirection = FVector2D::ZeroVector;
-	UE_LOG(LogPlayerShipPawn, Log, TEXT("APlayerShipPawn::GamepadMoveCompleted - MovementDirection: %s"), *MovementDirection.ToString());
+	UE_LOG(LogPlayerShipPawnInput, Verbose, TEXT("APlayerShipPawn::GamepadMoveCompleted - MovementDirection: %s"), *MovementDirection.ToString());
 }
 
 void APlayerShipPawn::GamepadAimTriggered(const FInputActionValue& InputActionValue)
 {
 	FVector2D AimTriggeredInput = InputActionValue.Get<FVector2D>();
 	AimingDirection = AimTriggeredInput.GetSafeNormal();
-	UE_LOG(LogPlayerShipPawn, Log, TEXT("APlayerShipPawn::GamepadAimTriggered - %s"), *AimingDirection.ToString());
+	UE_LOG(LogPlayerShipPawnInput, Verbose, TEXT("APlayerShipPawn::GamepadAimTriggered - %s"), *AimingDirection.ToString());
 }
 
 void APlayerShipPawn::GamepadAimCompleted(const FInputActionValue& InputActionValue)
 {
 	AimingDirection = FVector2D::ZeroVector;
-	UE_LOG(LogPlayerShipPawn, Log, TEXT("APlayerShipPawn::GamepadAimCompleted - AimingDirection: %s"), *AimingDirection.ToString());
+	UE_LOG(LogPlayerShipPawnInput, Verbose, TEXT("APlayerShipPawn::GamepadAimCompleted - AimingDirection: %s"), *AimingDirection.ToString());
 }
 
 void APlayerShipPawn::Fire(const FInputActionValue& InputActionValue)
 {
 	bool FireInput = InputActionValue.Get<bool>();
-	UE_LOG(LogPlayerShipPawn, Log, TEXT("APlayerShipPawn::Fire - FireInput: %d"), FireInput);
+	UE_LOG(LogPlayerShipPawnInput, Verbose, TEXT("APlayerShipPawn::Fire - FireInput: %d"), FireInput);
 }
 
