b076c5cabaed94ddc55ba555381184c1af62327c
RSkala <RichardSkala@gmail.com>
2024-07-30 11:19:17 -0700

    color cycling in main menu text

--- Source/SpaceShooter02/Private/UI/MainMenuWidget.cpp
+++ Source/SpaceShooter02/Private/UI/MainMenuWidget.cpp
@@ -2,3 +2,58 @@
 
 #include "UI/MainMenuWidget.h"
 
+#include "Components/TextBlock.h"
+
+void UMainMenuWidget::OnTick(float DeltaTime)
+{
+	// Note: This is set to update from the MainMenu level blueprint graph
+	UpdateMainMenuTitleTextColor(DeltaTime);
+}
+
+void UMainMenuWidget::UpdateMainMenuTitleTextColor(float DeltaTime)
+{
+	// Update color hue shift
+	ColorCyclingTimer += DeltaTime / SecondsPerCyclingMode;
+	FLinearColor LerpedColor(FLinearColor::White);
+
+	switch (ColorCyclingMode)
+	{
+		case EColorCyclingMode::RedToGreen:
+			LerpedColor = FMath::Lerp(FLinearColor::Red, FLinearColor::Green, ColorCyclingTimer);
+			break;
+
+		case EColorCyclingMode::GreenToBlue:
+			LerpedColor = FMath::Lerp(FLinearColor::Green, FLinearColor::Blue, ColorCyclingTimer);
+			break;
+
+		case EColorCyclingMode::BlueToRed:
+			LerpedColor = FMath::Lerp(FLinearColor::Blue, FLinearColor::Red, ColorCyclingTimer);
+			break;
+
+		default:
+			break;
+	}
+
+	if (MainMenuTitleText != nullptr)
+	{
+		MainMenuTitleText->SetColorAndOpacity(FSlateColor(LerpedColor));
+	}
+
+	if (ColorCyclingTimer >= 1.0f)
+	{
+		SwitchToNextColorCyclingMode();
+		ColorCyclingTimer = 0.0f;
+	}
+}
+
+void UMainMenuWidget::SwitchToNextColorCyclingMode()
+{
+	uint8 NextColorCyclingMode = ((uint8)ColorCyclingMode + 1) % (uint8)EColorCyclingMode::NumColorCyclingModes;
+	ColorCyclingMode = (EColorCyclingMode)NextColorCyclingMode;
+
+	//uint8 NextColorCyclingMode = (uint8)ColorCyclingMode + 1;
+	//if (NextColorCyclingMode >= (uint8)EColorCyclingMode::NumColorCyclingModes)
+	//{
+	//	NextColorCyclingMode = (uint8)EColorCyclingMode::RedToGreen;
+	//}
+}
--- Source/SpaceShooter02/Public/UI/MainMenuWidget.h
+++ Source/SpaceShooter02/Public/UI/MainMenuWidget.h
@@ -6,9 +6,39 @@
 #include "Blueprint/UserWidget.h"
 #include "MainMenuWidget.generated.h"
 
+UENUM(BlueprintType)
+enum class EColorCyclingMode : uint8
+{
+	RedToGreen,
+	GreenToBlue,
+	BlueToRed,
+	NumColorCyclingModes UMETA(Hidden)
+};
+ENUM_RANGE_BY_COUNT(EColorCyclingMode, EColorCyclingMode::NumColorCyclingModes); // This is REQUIRED in order to iterate over it in C++ (i.e. TEnumRange)
+
 UCLASS()
 class SPACESHOOTER02_API UMainMenuWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	UFUNCTION(BlueprintCallable)
+	void OnTick(float DeltaTime);
+
+protected:
+	void UpdateMainMenuTitleTextColor(float DeltaTime);
+	void SwitchToNextColorCyclingMode();
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (BindWidgetOptional))
+	TObjectPtr<class UTextBlock> MainMenuTitleText;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	EColorCyclingMode ColorCyclingMode = EColorCyclingMode::RedToGreen;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	float ColorCyclingTimer = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float SecondsPerCyclingMode = 1.0f; // How many seconds a color-cycling mode will last
 };
