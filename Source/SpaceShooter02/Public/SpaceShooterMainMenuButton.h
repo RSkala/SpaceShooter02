c6beafb655c60a02b45c8b6d174cdbac21ead0d5
RSkala <RichardSkala@gmail.com>
2024-07-29 21:57:19 -0700

    More main menu implementation

---
+++ Source/SpaceShooter02/Public/SpaceShooterMainMenuButton.h
@@ -0,0 +1,14 @@
+// Copyright 2024 Richard Skala
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SpaceShooterMainMenuButton.generated.h"
+
+UCLASS()
+class SPACESHOOTER02_API USpaceShooterMainMenuButton : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
