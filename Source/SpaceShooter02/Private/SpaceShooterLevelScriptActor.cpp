44673254d9d6deab44fa9bd370074c95de691603
RSkala <RichardSkala@gmail.com>
2024-07-31 17:06:35 -0700

    Add main menu to viewport when the game level loads

--- Source/SpaceShooter02/Private/SpaceShooterLevelScriptActor.cpp
+++ Source/SpaceShooter02/Private/SpaceShooterLevelScriptActor.cpp
@@ -2,3 +2,40 @@
 
 #include "SpaceShooterLevelScriptActor.h"
 
+#include "Blueprint/WidgetBlueprintLibrary.h"
+#include "Kismet/GameplayStatics.h"
+
+#include "UI/MainMenuScreen.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogSpaceShooterLevelScript, Log, All)
+
+void ASpaceShooterLevelScriptActor::BeginPlay()
+{
+	Super::BeginPlay();
+	StartMainMenu();
+}
+
+void ASpaceShooterLevelScriptActor::StartMainMenu()
+{
+	if (bSkipMainMenuLoad)
+	{
+		UE_LOG(LogSpaceShooterLevelScript, Warning, TEXT("Skipping Main Menu load"));
+		return;
+	}
+
+	if (ensure(MainMenuScreenClass != nullptr))
+	{
+		UWorld* World = GetWorld();
+		APlayerController* PlayerController = UGameplayStatics::GetPlayerController(World, 0);
+
+		UUserWidget* NewWidget = UWidgetBlueprintLibrary::Create(World, MainMenuScreenClass, PlayerController);
+		MainMenuScreen = Cast<UMainMenuScreen>(NewWidget);
+
+		//FInputModeGameAndUI InputMode;
+		//InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::LockAlways);
+		//InputMode.SetHideCursorDuringCapture(false);
+		//PlayerController->SetInputMode(InputMode);
+		//PlayerController->SetShowMouseCursor(true);
+		MainMenuScreen->AddToViewport(0);
+	}
+}
--- Source/SpaceShooter02/Public/SpaceShooterLevelScriptActor.h
+++ Source/SpaceShooter02/Public/SpaceShooterLevelScriptActor.h
@@ -10,5 +10,26 @@ UCLASS()
 class SPACESHOOTER02_API ASpaceShooterLevelScriptActor : public ALevelScriptActor
 {
 	GENERATED_BODY()
-	
+
+protected:
+	virtual void BeginPlay() override;
+
+	void StartMainMenu();
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<class UUserWidget> MainMenuScreenClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<class UUserWidget> PlayerShipSelectScreenClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<class UUserWidget> GameOverScreenClass;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly)
+	TObjectPtr<class UMainMenuScreen> MainMenuScreen;
+
+	// If true, skips loading the main menu and goes right into gameplay
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bSkipMainMenuLoad = false;
 };
