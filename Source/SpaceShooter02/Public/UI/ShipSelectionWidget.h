4459b9406036bc5bd416c9992fb66fe1f84d0ae9
RSkala <RichardSkala@gmail.com>
2024-08-03 19:27:23 -0700

    Added first pass ship selection widget

---
+++ Source/SpaceShooter02/Public/UI/ShipSelectionWidget.h
@@ -0,0 +1,14 @@
+// Copyright 2024 Richard Skala
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ShipSelectionWidget.generated.h"
+
+UCLASS()
+class SPACESHOOTER02_API UShipSelectionWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
