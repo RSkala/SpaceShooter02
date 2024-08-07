4089c3d0e3accc2b5a59af2cd739e41282c381ec
RSkala <RichardSkala@gmail.com>
2024-08-06 18:28:12 -0700

    Update ExplosionBase.h

--- Source/SpaceShooter02/Public/ExplosionBase.h
+++ Source/SpaceShooter02/Public/ExplosionBase.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Actor.h"
 #include "ExplosionBase.generated.h"
 
+// Class for playing an enemy explosion PaperFlipbook animation.
+// Using a PaperFlipBookComponent is the only way to be notified when a
+// PaperFlipbook animation finishes, unless you use the PaperZD plugin.
 UCLASS(Abstract)
 class SPACESHOOTER02_API AExplosionBase : public AActor
 {
