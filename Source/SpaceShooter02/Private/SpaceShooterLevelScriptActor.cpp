858b221ae19b845827f9a33d52fa111a042fbea2
RSkala <RichardSkala@gmail.com>
2024-08-13 08:51:51 -0700

    Removed unnecessary includes

--- Source/SpaceShooter02/Private/SpaceShooterLevelScriptActor.cpp
+++ Source/SpaceShooter02/Private/SpaceShooterLevelScriptActor.cpp
@@ -2,11 +2,10 @@
 
 #include "SpaceShooterLevelScriptActor.h"
 
-#include "Blueprint/WidgetBlueprintLibrary.h"
-#include "Kismet/GameplayStatics.h"
+//#include "Blueprint/WidgetBlueprintLibrary.h"
+//#include "Kismet/GameplayStatics.h"
 
 //#include "SpaceShooterGameState.h"
-#include "UI/MainMenuScreen.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogSpaceShooterLevelScript, Log, All)
 
